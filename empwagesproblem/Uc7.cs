fe0cabe6258100798883828dd3b9a478319d9730
santoshinawkhare <gunjannawkhare1@gmail.com>
2021-10-28 16:45:00 +0530

    Add UC7

---
+++ empwagesproblem/Uc7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    class Uc7
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+        public void ComputeEmpWage()
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                totalWorkingDays++;
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " EmpHrs: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Monthly Employee Wage :" + totalEmpWage);
+        }
+
+    }
+}
