d095c289f0ebe026013882cd3a5abba1f9ba529b
santoshinawkhare <gunjannawkhare1@gmail.com>
2021-10-31 22:57:16 +0530

    adding uc11.cs

---
+++ empwagesproblem/IEmployeeWageUC11.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    interface IEmployeeWageUC11
+    {
+        public void addCompany(string company, int empRatePerHr, int numWorkingDays, int maxHrsInMonth);
+        public void ComputeEmpWage();
+    }
+}
